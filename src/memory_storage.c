fe8203d417349b0c0135d6bbdbb5b4c429d895b8
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2025-06-26 12:39:46 +0100

    added more instruction in the main.c file switch cases

--- src/memory_storage.c
+++ src/memory_storage.c
@@ -1,5 +1,6 @@
 #include "register.c"
 #include <stdio.h>
+#include"include/source.h"
 #define max_mem_size (1 << 16)
-__uint16_t memory[max_mem_size];// 65536 location
-__uint16_t reg[R_COUNT];
+uint16_t memory[max_mem_size];// 65536 location
+uint16_t reg[R_COUNT];
